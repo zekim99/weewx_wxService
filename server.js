082b4239d8791c25d2690bcad23fa41a3a6e25dd
zekim99 <mike@fishwheel.com>
2020-01-10 22:47:01 -0800

    Refactored for 2048 byte cert, utilizing helmet

--- server.js
+++ server.js
@@ -1,81 +1,80 @@
-//var http = require('http');
-var https = require('https');
+var express = require('express'),
+    https = require('https'),
+    fs = require('fs'),
+    helmet = require('helmet'),
+    pjson = require('./package.json');
+
 const PORT=8080;
+const process = require('process');
+
 var _fullBody = '';
 var wx;
+
+console.log("weewx_server %s", pjson.version)
+
+var app = express();
+
+app.use(helmet());
+
+// set up for ssl
 const options = {
-  cert: fs.readFileSync("/etc/apache2/ssl/fe2b4db8c986695b.crt"),
-  key: fs.readFileSync("/etc/apache2/ssl/private/www_sahaleeweather_com.key") 
+  cert: fs.readFileSync('/etc/apache2/ssl/fe2b4db8c986695b.crt'),
+  key: fs.readFileSync('/etc/apache2/ssl/private/www_sahaleeweather_com.key'),
+  dhparam: fs.readFileSync('/etc/apache2/ssl/dh-strong.pem')
 }
 
-//We need a function which handles requests and send response
-function handleRequest(req, res){
-
-    switch(req.url) {
-      case '/':
-        console.log("[501] " + req.method + " to " + req.url);
-        res.writeHead(501, "Not implemented", {'Content-Type': 'text/html'});
-        res.end('<html><head><title>501 - Not implemented</title></head><body><h1>Not implemented!</h1></body></html>');
-        break;
-
-      case '/wxupdate':
-        if (req.method == 'POST') {
-          console.log("[200] " + req.method + " to " + req.url);
-
-          _fullBody = '';
-          req.on('data', function(chunk) {
-            console.log("Received body data:");
-            _fullBody += chunk.toString();
-            console.log(_fullBody);
-          });
-
-          req.on('end', function() {
-            // empty 200 OK response for now
-            res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-            res.end();
-          });
-
-        } else {
-          console.log("[405] " + req.method + " to " + req.url);
-          res.writeHead(405, "Method not supported", {'Content-Type': 'text/html'});
-          res.end('<html><head><title>405 - Method not supported</title></head><body><h1>Method not supported.</h1></body></html>');
-        }
-        break;
-
-      case '/wx':
-        if (req.method == 'GET') {
-          console.log("Request for data:");
-          res.writeHead(200, "OK", {
-            'Content-Type': 'text/json',
-            'Cache-Control': 'no-cache, no-store, must-revalidate',
-            'Pragma': 'no-cache',
-            'Expires': 0
-          });
-          res.write(_fullBody);
-          res.end();
-        } else {
-          console.log("[405] " + req.method + " to " + req.url);
-          res.writeHead(405, "Method not supported", {'Content-Type': 'text/html'});
-          res.end('<html><head><title>405 - Method not supported</title></head><body><h1>Method not supported.</h1></body></html>');
-        }
-        break;
-
-      default:
-        res.writeHead(404, "Not found", {'Content-Type': 'text/html'});
-        res.end('<html><head><title>404 - Not found</title></head><body><h1>Not found.</h1></body></html>');
-        console.log("[404] " + req.method + " to " + req.url);
-      };
-}
+// process handler
+process.on('uncaughtExeption', (code) => {
+  process.exit(1);
+});
 
 
+// requesting weather data
+app.get('/wx', function (req, res) {
+  console.log('Request for data:');
+  res.writeHead(200, 'OK', {
+    'Content-Type': 'text/json',
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': 0
+  });
+  res.write(_fullBody);
+  res.end();
+});
+
+
+// posting weather data
+app.post('/wxupdate', function(req, res) {
+  console.log('[200] ' + req.method + ' to ' + req.url);
+
+  _fullBody = '';
+
+  req.on('data', function(chunk) {
+    console.log('Received body data:');
+    _fullBody += chunk.toString();
+    console.log(_fullBody);
+   });
+
+  req.on('end', function() {
+    // empty 200 OK response for now
+    res.writeHead(200, 'OK', {'Content-Type': 'text/html'});
+    res.end();
+  });
+});
+
+// reject root
+app.get('/', function (req, res) {
+  console.log('[501] ' + req.method + ' to ' + req.url);
+  res.writeHead(501, 'Not implemented', {'Content-Type': 'text/html'});
+  res.end('<html><head><title>501 - Not implemented</title></head><body><h1>Not implemented!</h1></body></html>');
+});
+
+//app.listen(PORT);
 
 //Create a server
-//var server = http.createServer(handleRequest);
-var server = https.createServer(handleRequest);
+var server = https.createServer(options, app);
 
 //Lets start our server
 server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    //console.log("Server listening on: http://localhost:%s", PORT);
     console.log("Server listening on: https://localhost:%s", PORT);
 });
